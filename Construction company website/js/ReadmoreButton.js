fdbfb767e3a62b385292186af60c5a74c0a0d5d5
Deiaa Mohamed <moamn100g@gmail.com>
2025-01-26 02:48:21 +0200

    alot of changes i can't remember acutally

--- Construction company website/js/ReadmoreButton.js
+++ Construction company website/js/ReadmoreButton.js
@@ -2,9 +2,9 @@ function toggleText(id, btn) {
     const text = document.getElementById(id);
     if (text.classList.contains('text-truncate-3')) {
         text.classList.remove('text-truncate-3');
-        btn.innerText = 'Show Less';
+        btn.innerText = 'اعرض اقل';
     } else {
         text.classList.add('text-truncate-3');
-        btn.innerText = 'Read More';
+        btn.innerText = 'اقرأ المزيد';
     }
 }
